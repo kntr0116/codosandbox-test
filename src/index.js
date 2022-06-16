17d3cc80d08ce6a93c7343598a20d476c32c27c8
kntr0116 <88098308+kntr0116@users.noreply.github.com>
2022-06-16 22:22:21 +0900

    plactice

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-import "./styles.css";
+/**
+ * 三項演算子
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//ある条件 ? 条件がtrueのとき : 条件がfalseのとき
+
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+
+// const formattednum = typeof num  === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattednum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています' : '許容範囲内です';
+// }
+
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+
+//  const flag1 = true;
+//  const flag2 = false;
+
+//  if (flag1 || flag2) {
+//    console.log("1か2はtrueです");
+//  }
+
+//  if (flag1 && flag2) {
+//   console.log("1も2もtrueです");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 1;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "なにか設定されました";
+console.log(fee2);
